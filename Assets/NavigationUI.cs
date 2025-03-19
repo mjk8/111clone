cea1ae67da89ab403a76df6479397960a34eda68
mjk8 <mjktt8@gmail.com>
2025-03-19 17:47:07 +0900

    Navigation UI logic

--- Assets/NavigationUI.cs
+++ Assets/NavigationUI.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -9,32 +10,67 @@ public class NavigationUI : MonoBehaviour
     [SerializeField] Transform _gameplayInteractionPanel;
     [SerializeField] Transform _extraInteractionPanel;
 
+    [SerializeField] List<GameObject> _panels;
     [SerializeField] TMP_Text jusulSummonCost; 
         
-    int _isExtraPanelActive = -1;
-    
-    
-    //만약 extra panel이 활성화 되면 _gameplayInteractionPanel을 _extraInteractionPanel의 RectTransform Height만큼 위로 옮겨야함.
-    public void OnExtraPanelActive()
+    int _currentPanel = -1;
+
+    void Start()
     {
-        if (_isExtraPanelActive<0)
+        foreach (var p in _panels)
         {
-            _gameplayInteractionPanel.position += new Vector3(0, _extraInteractionPanel.GetComponent<RectTransform>().rect.height, 0);
-        }
-        else
-        {
-            _gameplayInteractionPanel.position -= new Vector3(0, _extraInteractionPanel.GetComponent<RectTransform>().rect.height, 0);
+            p.SetActive(false);
         }
     }
 
+    //만약 extra panel이 활성화 되면 _gameplayInteractionPanel을 _extraInteractionPanel의 RectTransform Height만큼 위로 옮겨야함.
+    public void ChangePanel(int panelNum)
+    {
+        if (_currentPanel<0)
+        {
+            _gameplayInteractionPanel.position += new Vector3(0, _extraInteractionPanel.GetComponent<RectTransform>().rect.height, 0);
+            _currentPanel = panelNum;
+            _panels[_currentPanel].SetActive(true);
+        }
+        
+        else if (_currentPanel>=0 && panelNum<0)
+        {
+            _gameplayInteractionPanel.position -= new Vector3(0, _extraInteractionPanel.GetComponent<RectTransform>().rect.height, 0);
+            _panels[_currentPanel].SetActive(false);
+            _currentPanel = panelNum;
+        }
+            
+        else
+        {
+            _panels[_currentPanel].SetActive(false);
+            _currentPanel = panelNum;
+            _panels[_currentPanel].SetActive(true);
+        }
+    }
+    
+    #region Navigation Buttons
     public void BountyButton()
     {
-        
+        if (_currentPanel != 0)
+        {
+            ChangePanel(0);
+        }
+        else
+        {
+            ChangePanel(-1);
+        }
     }
 
     public void ShinsuButton()
     {
-        
+        if (_currentPanel != 1)
+        {
+            ChangePanel(1);
+        }
+        else
+        {
+            ChangePanel(-1);
+        }
     }
     
     public void GetJusulButton()
@@ -45,11 +81,26 @@ public class NavigationUI : MonoBehaviour
 
     public void EnhanceButton()
     {
-        
+        if (_currentPanel != 2)
+        {
+            ChangePanel(2);
+        }
+        else
+        {
+            ChangePanel(-1);
+        }
     }
     
     public void MineButton()
     {
-        
+        if (_currentPanel != 3)
+        {
+            ChangePanel(3);
+        }
+        else
+        {
+            ChangePanel(-1);
+        }
     }
+    #endregion
 }
